69b2a7b1317de071d0f310bab76d414ad2f0f4dc
Ariel Fischer <arieldf@gmail.com>
2017-04-15 23:45:21 +0200

    Fix noRender

--- src/components/component-playground.js
+++ src/components/component-playground.js
@@ -38,7 +38,7 @@ const ComponentPlayground = ({
       <Playground
         codeText={(code || defaultCode).trim()}
         scope={{ React, Component, render, ...scope }}
-        noRender,
+        noRender
         theme={useDarkTheme ? "material" : "elegant"}
       />
     </ComponentPlaygroundContainer>
